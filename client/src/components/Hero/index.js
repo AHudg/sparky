98b86402631ddfb59d6e044b00c5d3b051134b35
Andrew Hudgins <andrewhudgins15@gmail.com>
2023-10-16 19:26:29 -0500

    Completed landing hero scalability

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -36,12 +36,12 @@ function Hero({
           <img src={accent} className={accentClass} />
         </div>
         <div className="col-12 col-lg-5 row text-center justify-content-center coa">
-          <p className="col-12">
+          <p>
             Cheering you on for your next DIY, we illuminate your interest and
             encourage you to get out and go do! Here is your spark, now ignite
             your inspiration.
           </p>
-          <a className="col-6 mt-4 buttonBlog" href="/blog">
+          <a className="col-6 mt-4" href="/blog">
             Explore!
           </a>
         </div>
